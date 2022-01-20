010fc092ebc2b65a53e9919afc35499db86e276d
77。 <393103982@qq.com>
2022-01-20 14:48:12 +0800

    Update warning plugin

--- Sources/MoyaNetwork/NetworkX.swift
+++ Sources/MoyaNetwork/NetworkX.swift
@@ -47,3 +47,33 @@ public func += <K,V> (left: inout Dictionary<K,V>, right: Dictionary<K,V>?) {
         left.updateValue(value, forKey: key)
     }
 }
+
+struct View {
+    
+    static var keyWindow: UIWindow? {
+        if #available(iOS 13.0, *) {
+            return UIApplication.shared.connectedScenes
+                .filter { $0.activationState == .foregroundActive }
+                .first(where: { $0 is UIWindowScene })
+                .flatMap({ $0 as? UIWindowScene })?.windows
+                .first(where: \.isKeyWindow)
+        } else {
+            return UIApplication.shared.keyWindow
+        }
+    }
+    
+    static var topViewController: UIViewController? {
+        var vc = keyWindow?.rootViewController
+        if let presentedController = vc as? UITabBarController {
+            vc = presentedController.selectedViewController
+        }
+        while let presentedController = vc?.presentedViewController {
+            if let presentedController = presentedController as? UITabBarController {
+                vc = presentedController.selectedViewController
+            } else {
+                vc = presentedController
+            }
+        }
+        return vc
+    }
+}
--- Sources/MoyaPlugins/Warning/NetworkWarningPlugin.swift
+++ Sources/MoyaPlugins/Warning/NetworkWarningPlugin.swift
@@ -13,7 +13,30 @@ import MBProgressHUD
 /// Indicator plug-in, the plug-in has been set for global use
 public final class NetworkWarningPlugin {
     
-    public init() { }
+    // 用于区分其他插件等是否使用过`MBProgressHUD`
+    // Used to distinguish whether other plugins have used `MBProgressHUD`
+    public static let WarningHUDT = 5200124
+    
+    /// Whether to display the Window again, the default is YES
+    let displayInWindow: Bool
+    /// Show time
+    let displayTime: TimeInterval
+    
+    /// 是否会覆盖上次的错误展示，
+    /// 如果上次错误展示还在，新的错误展示是否需要覆盖上次
+    /// Whether it will overwrite the last error display,
+    /// If the last error display is still there, whether the new error display needs to overwrite the last one
+    let coverLastHUD: Bool
+    
+    /// 更改`hud`相关配置闭包
+    /// Change `hud` related configuration closures.
+    public var changeHud: ((_ hud: MBProgressHUD) -> Void)? = nil
+    
+    public init(in window: Bool = true, time: TimeInterval = 1, cover: Bool = true) {
+        self.displayInWindow = window
+        self.displayTime = time
+        self.coverLastHUD = cover
+    }
 }
 
 extension NetworkWarningPlugin: PluginSubType {
@@ -23,7 +46,40 @@ extension NetworkWarningPlugin: PluginSubType {
         case .success(_):
             break
         case let .failure(error):
-            print(error.localizedDescription)
+            self.showText(error.localizedDescription)
+        }
+    }
+}
+
+extension NetworkWarningPlugin {
+    
+    private func showText(_ text: String) {
+        DispatchQueue.main.async {
+            guard let view = self.displayInWindow ? RxNetworks.View.keyWindow :
+                    RxNetworks.View.topViewController?.view else { return }
+            if let _hud = MBProgressHUD.forView(view), _hud.tag == NetworkWarningPlugin.WarningHUDT {
+                if self.coverLastHUD {
+                    _hud.detailsLabel.text = text
+                    _hud.hide(animated: true, afterDelay: self.displayTime)
+                }
+                return
+            }
+            let hud = MBProgressHUD.showAdded(to: view, animated: true)
+            hud.tag = NetworkWarningPlugin.WarningHUDT
+            hud.mode = MBProgressHUDMode.text
+            hud.animationType = MBProgressHUDAnimation.zoom
+            hud.removeFromSuperViewOnHide = true
+            hud.bezelView.style = MBProgressHUDBackgroundStyle.solidColor
+            hud.bezelView.color = UIColor.black.withAlphaComponent(0.7)
+            hud.bezelView.layer.cornerRadius = 14
+            hud.detailsLabel.text = text
+            hud.detailsLabel.numberOfLines = 0
+            hud.detailsLabel.textColor = UIColor.white
+            hud.detailsLabel.font = UIFont.systemFont(ofSize: 16)
+            hud.hide(animated: true, afterDelay: self.displayTime)
+            if let changeHud = self.changeHud {
+                changeHud(hud)
+            }
         }
     }
 }
