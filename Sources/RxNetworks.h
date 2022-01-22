f83e779982135f7501bb719a5856176c99443af7
77。 <393103982@qq.com>
2022-01-22 22:38:34 +0800

    Add rx network

---
+++ Sources/RxNetworks.h
@@ -0,0 +1,42 @@
+//
+//  RxNetworks.h
+//
+//  Copyright (c) 2021 Condy https://github.com/YangKJ
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// 该组件主要是基于 RxSwift + Moya 搭建响应式数据绑定网络API架构
+
+// 部分数据参考来源于网络，汇总起来好使用，慢慢补充完善吧！！！
+// 如果觉得好用，希望您能STAR支持，你的 ⭐️ 是我持续更新的动力!
+// 传送门：https://github.com/YangKJ/RxNetworks <备注：快捷打开浏览器命令，command + 鼠标左键>
+
+
+// This component mainly builds responsive data binding network API architecture based on RxSwift + Moya.
+
+// Part of the data reference comes from the Internet,
+// And it is easy to use when summarizing it, so let's add it slowly.
+// If you find it easy to use, I hope you can support STAR. Your ⭐️ is my motivation for updating!
+// Portal: https://github.com/YangKJ/RxNetworks <Note: Open the browser command quickly, command + left mouse button>
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double RxNetworksVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char RxNetworksVersionString[];
