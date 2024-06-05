08bd91039856cf29753cf2fdf4aeef6f060daf34
Condy <yangkj310@gmail.com>
2024-06-05 14:43:55 +0800

    Update mapping codable

--- Codable/MappingCodable.swift
+++ Codable/MappingCodable.swift
@@ -13,7 +13,63 @@ public protocol MappingCodable: Codable {
 }
 
 extension MappingCodable {
+    
     public static var codingKeys: [ReplaceKeys] {
         []
+    }
+    
+    public func toData(prettyPrint: Bool = false) -> Data? {
+        let encoder = JSONEncoder()
+        if prettyPrint {
+            encoder.outputFormatting = .prettyPrinted
+        }
+        let mapKeys = Self.codingKeys
+        if !mapKeys.isEmpty {
+            let mapping = mapKeys.toEncoderingMappingKeys
+            encoder.keyEncodingStrategy = .custom({ codingPath in
+                let key = codingPath.last!.stringValue
+                if let mapped = mapping[key] {
+                    return AnyCodingKey(stringValue: mapped)
+                } else {
+                    return AnyCodingKey(stringValue: key)
+                }
+            })
+        }
+        guard let jsonData = try? encoder.encode(self) else {
+            return nil
+        }
+        return jsonData
+    }
+    
+    public func toDictionary() -> [String : Any] {
+        guard let data = toData(),
+              let dict = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? Dictionary<String, Any> else {
+            return [:]
+        }
+        return dict
+    }
+    
+    public func toJSONString() -> String? {
+        guard let jsonData = toData() else {
+            return nil
+        }
+        return String(decoding: jsonData, as: UTF8.self)
+    }
+}
+
+extension Collection where Element: MappingCodable {
+    
+    public func toJSONString() -> String? {
+        let array = toJSONArray()
+        guard let data = try? JSONSerialization.data(withJSONObject: array, options: []) else {
+            return nil
+        }
+        return NSString(data: data, encoding: String.Encoding.utf8.rawValue) as? String
+    }
+    
+    public func toJSONArray() -> [String] {
+        self.compactMap {
+            $0.toJSONString()
+        }
     }
 }
