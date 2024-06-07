7455c8c47e0adace453e816953e70cfe244851fb
Condy <yangkj310@gmail.com>
2024-06-07 12:48:06 +0800

    Update cache plugin

--- NetworkCachePlugin.podspec
+++ NetworkCachePlugin.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'NetworkCachePlugin'
-  s.version          = '1.0.6'
+  s.version          = '1.0.7'
   s.summary          = 'Network Cache Plugin Library.'
   
   # This description is used to generate tags and improve search results.
--- RxNetworks/Modules/CustomCache/CustomCacheViewModel.swift
+++ RxNetworks/Modules/CustomCache/CustomCacheViewModel.swift
@@ -49,3 +49,20 @@ extension CustomCacheViewModel {
             .observe(on: MainScheduler.instance)
     }
 }
+
+extension CacheX.Storage: CacheConvertable where T == Moya.Response {
+    
+    public func readResponse(forKey key: String) throws -> Moya.Response? {
+        let key = CryptoType.md5.encryptedString(with: key)
+        return self.fetchCached(forKey: key, options: .diskAndMemory)
+    }
+    
+    public func saveResponse(_ response: Moya.Response, forKey key: String) throws {
+        let key = CryptoType.md5.encryptedString(with: key)
+        self.storeCached(response, forKey: key, options: .diskAndMemory)
+    }
+    
+    public func clearAllResponses() {
+        self.removedDiskAndMemoryCached()
+    }
+}
